67b118ddd9d8f85c614ad826e727eaf3f83b72c6
stanchung <stan820624@gmail.com>
2024-02-07 21:21:25 +0800

    test

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 import os
 import sys
 from argparse import ArgumentParser
 
 from flask import Flask, request, abort
-from linebot import (
-    WebhookParser
+from linebot.v3 import (
+     WebhookHandler
 )
 from linebot.v3.exceptions import (
     InvalidSignatureError
@@ -34,7 +33,7 @@ if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
-parser = WebhookParser(channel_secret)
+handler = WebhookHandler(channel_secret)
 
 configuration = Configuration(
     access_token=channel_access_token
@@ -43,34 +42,32 @@ configuration = Configuration(
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # parse webhook body
+    # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessageContent):
-            continue
-        with ApiClient(configuration) as api_client:
-            line_bot_api = MessagingApi(api_client)
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text=event.message.text)]
-                )
-            )
-
     return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def message_text(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
 
 
 if __name__ == "__main__":
